2f1af5398701c0d3b2572fffb5f9836adb0da230
Ratul Raha <gkdratul@gmail.com>
2026-04-19 16:14:07 +0600

    refactored code

---
+++ src/PulsarPlay/Models/Models.cs
@@ -0,0 +1,48 @@
+using System.Collections.Generic;
+
+namespace PulsarPlay.Models;
+
+public class PortInfo
+{
+    public string Port { get; set; } = "";
+    public string Pid { get; set; } = "";
+    public string Process { get; set; } = "";
+    public bool IsActive { get; set; }
+    public string Command { get; set; } = "";
+    public string Folder { get; set; } = "";
+}
+
+public class ProjectInfo
+{
+    public string Name { get; set; } = "";
+    public string Path { get; set; } = "";
+    public string Port { get; set; } = "";
+    public string Command { get; set; } = "";
+    public string OpenCodeCommand { get; set; } = "";
+    public bool IsActive { get; set; }
+    public string Pid { get; set; } = "";
+    public bool IsParent { get; set; }
+    public List<ProjectInfo> Children { get; set; } = new();
+}
+
+public class CustomCommand
+{
+    public string Name { get; set; } = "";
+    public string Command { get; set; } = "";
+}
+
+public class ProcessInfo
+{
+    public string Name { get; set; } = "";
+    public double Percent { get; set; }
+}
+
+public class AppSettings
+{
+    public int RamWarning { get; set; } = 80;
+    public int RamCritical { get; set; } = 90;
+    public int CpuWarning { get; set; } = 80;
+    public int CpuCritical { get; set; } = 95;
+    public bool StartWithWindows { get; set; }
+    public bool MinimizeToTray { get; set; } = true;
+}
